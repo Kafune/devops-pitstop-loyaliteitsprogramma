8674df0136d60ecd385189520f1f8263a13d9f98
Kafune <likachung97@gmail.com>
2023-10-24 11:29:15 +0200

    #30 init back-end loyaliteitsprogramma

---
+++ src/LoyalitySystemAPI/Models/Loyalty.cs
@@ -0,0 +1,10 @@
+﻿namespace LoyalitySystemAPI.Models
+{
+    public class Loyalty
+    {
+        public int CustomerID { get; set; }
+        public string Points { get; set; }
+        public string Category { get; set; }
+
+    }
+}
