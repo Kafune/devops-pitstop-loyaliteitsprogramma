bb04146b6ea3dd046c122abf036b52927ebf9efa
Kafune <likachung97@gmail.com>
2023-10-30 11:34:38 +0100

    #36 baseimage to private registry

--- src/RebuildAllDockerImages.sh
+++ src/RebuildAllDockerImages.sh
@@ -21,7 +21,7 @@ docker build -t pitstop-dotnet-sdk-base:1.0 . -f dotnet-sdk-base-dockerfile
 docker build -t pitstop-dotnet-runtime-base:1.0 . -f dotnet-runtime-base-dockerfile
 
 # Build the .NET ASP.NET base image
-docker build -t pitstop-dotnet-aspnet-base:1.0 . -f dotnet-aspnet-base-dockerfile
+docker build -t ghcr.io/hanaim-devops/pitstop-dotnet-aspnet-base:1.0 . -f dotnet-aspnet-base-dockerfile
 
 # Rebuild all the services that have changes
 # If you want to (re)build only a specific service, go to the src folder and execute `docker-compose build <servicename-lowercase>`
