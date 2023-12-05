2048a361a5e2444c8db369a4765654e834a42079
Wesley Lozekoot <74497659+WesleyLoz@users.noreply.github.com>
2023-12-05 11:18:06 +0100

    Update RebuildAllDockerImages.sh

--- src/RebuildAllDockerImages.sh
+++ src/RebuildAllDockerImages.sh
@@ -16,10 +16,10 @@ docker volume create --name=rabbitmqdata
 docker volume create --name=grafanadata
 
 # Build the .NET SDK base image that contains the Directory.Packages.props file so it is used when restoring the NuGet packages
-docker build -t pitstop-dotnet-sdk-base:1.0 . -f dotnet-sdk-base-dockerfile
+docker build -t ghcr.io/pitstop-dotnet-sdk-base:1.0 . -f dotnet-sdk-base-dockerfile
 
 # Build the .NET runtime base image
-docker build -t pitstop-dotnet-runtime-base:1.0 . -f dotnet-runtime-base-dockerfile
+docker build -t ghcr.io/pitstop-dotnet-runtime-base:1.0 . -f dotnet-runtime-base-dockerfile
 
 # Build the .NET ASP.NET base image
 docker build -t ghcr.io/hanaim-devops/pitstop-dotnet-aspnet-base:1.0 . -f dotnet-aspnet-base-dockerfile
